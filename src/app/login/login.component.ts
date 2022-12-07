f12460ffef694ad0e1c89d8bee4385fde4018aaa
NIJIN JOY <nijinjoy1999@gmail.com>
2022-12-07 23:52:16 +0530

    data binding

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { AsyncSubject } from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  acno:any
+  psw:any
+
+  aim= "Your Perfect Banking Partner"
+  data="Enter Account Number"
+  userDetails:any={
+    1000:{acno:1000,username:"Aju",password:123,balance:0},
+    1001:{acno:1001,username:"Abhi",password:123,balance:0},
+    1002:{acno:1002,username:"Vinu",password:123,balance:0},
+    1003:{acno:1003,username:"Liju",password:123,balance:0}
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+login(){
+  var acno=this.acno
+  var psw=this.psw
+  var userDetails=this.userDetails
+
+  if(acno in userDetails){
+    
+   if(psw==userDetails[acno]['password']){
+     alert("Login Success")
+   }
+   else{
+    alert("Incorrect Password")
+   }
+  }
+  else{
+    alert("User not exists")
+  }
+}
+acnochange(event:any){
+     this.acno=event.target.value
+}
+pswchange(event:any){
+  this.psw=event.target.value
+}
+
+
+
+}
